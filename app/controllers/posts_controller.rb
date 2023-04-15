b8c3689608c46d503a381580890ef1e969e08256
Yuko Horii <yuko.n@nakajimayuukonoMacBook-Air.local>
2023-04-15 18:08:09 +0900

    予定一覧ページと新規追加ページのビューの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,21 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
-
+    @post = Post.new(params.require(:post).permit(:title, :start, :finish, :is_allday, :memo))
+    if @post.save
+      flash[:notice] = "予定を新規追加しました"
+      redirect_to posts_path
+    else
+      flash[:notice] = "未入力の項目があります"
+      render "new"
+    end
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_13_121336) do
+ActiveRecord::Schema.define(version: 2023_04_15_065904) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2023_04_13_121336) do
     t.boolean "is_allday"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "memo"
   end
 
 end
