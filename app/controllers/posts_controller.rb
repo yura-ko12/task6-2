bf1e0a9fcb775ea890c8f33361aac0cebe4b3a87
Yuko Horii <yuko.n@nakajimayuukonoMacBook-Air.local>
2023-04-21 19:04:02 +0900

    バリデーションの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    
   end
 
   def new
@@ -13,24 +14,34 @@ class PostsController < ApplicationController
       flash[:notice] = "予定を新規追加しました"
       redirect_to posts_path
     else
-      flash[:notice] = "未入力の項目があります"
+      flash[:notice] = "内容に不備があります"
       render "new"
     end
   end
 
   def show
-
+    @post = Post.find(params[:id])
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
-
+    @post = Post.find(params[:id])
+    if @post.update(params.require(:post).permit(:title, :start, :finish, :is_allday, :memo))
+      flash[:notice] = "内容を更新しました"
+      redirect_to posts_path
+    else
+      flash[:notice] = "内容に不備があります"
+      render "edit"
+    end
   end
 
   def destroy
-
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "予定を削除しました"
+    redirect_to posts_path
   end
 end
