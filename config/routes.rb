8a2f5821bf83e5164bace7c33d95f8737c5230be
Yuko Horii <yuko.n@nakajimayuukonoMacBook-Air.local>
2023-04-21 21:59:52 +0900

    編集ページの修正とrootの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # get 'posts/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root 'posts#index'
   resources :posts
 end
